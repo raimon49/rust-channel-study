44f444cd02af2906fe279a71f562ca52b9887f27
raimon <raimon49@hotmail.com>
2022-01-05 20:19:19 +0900

    Add a annotate comment about 'mpsc' package

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::thread::spawn;
 use std::sync::mpsc::channel;
 
 fn main() {
+    // mpscはmulti-producer, single-consumer（複数の生産者、単一の消費者）を意味する
+    // std::sync::mpsc::sync_channelを使うと同期チャネルになる
     let (sender, receiver) = channel();
 
     let handle = spawn(move || {
