885d7f3def88cef49b8d6e2eb9caa69ea7cfa7c3
raimon <raimon49@hotmail.com>
2022-01-08 17:36:45 +0900

    Add a code about implement 'OffThreadExt' for iterator type

--- src/main.rs
+++ src/main.rs
@@ -30,4 +30,22 @@ fn main() {
     pub trait OffThreadExt: Iterator {
         fn off_thread(self) -> mpsc::IntoIter<Self::Item>;
     }
+
+    impl<T> OffThreadExt for T
+        where T: Iterator + Send + 'static,
+              T::Item: Send + 'static
+    {
+        fn off_thread(self) -> mpsc::IntoIter<Self::Item> {
+            let (sender, receiver) = mpsc::sync_channel(1024);
+            spawn(move || {
+                for item in self {
+                    if sender.send(item).is_err() {
+                        break;
+                    }
+                }
+            });
+
+            receiver.into_iter()
+        }
+    }
 }
