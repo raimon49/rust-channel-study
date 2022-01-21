46f563a8e4b71c934274188f1c77d16e15bb7e35
raimon <raimon49@hotmail.com>
2022-01-21 19:20:00 +0900

    Add a code about implement Iterator for SharedReceiver

--- src/main.rs
+++ src/main.rs
@@ -96,6 +96,15 @@ fn main() {
 
             #[derive(Clone)]
             pub struct SharedReceiver<T>(Arc<Mutex<Receiver<T>>>);
+
+            impl<T> Iterator for SharedReceiver<T> {
+                type Item = T;
+
+                fn next(&mut self) -> Option<T> {
+                    let guard = self.0.lock().unwrap();
+                    guard.recv().ok()
+                }
+            }
         }
     }
 }
